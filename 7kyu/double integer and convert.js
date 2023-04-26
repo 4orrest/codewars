7c7a9f98529da1f159cc53ce4a6ae8f14095be31
4orrest <76661750+4orrest@users.noreply.github.com>
2023-04-25 22:59:15 -0700

    added new 6 difficulty problem

---
+++ 7kyu/double integer and convert.js
@@ -0,0 +1,10 @@
+//Code as fast as you can! You need to double the integer and return it.
+
+
+//MY SOLUTION
+function doubleInteger(i) {
+    return i * 2;
+  }
+
+  //TOP RATED
+  const doubleInteger = i => 2 * i;
